d2d5bd3b6f8557df1f6541c4ba8953236ebbb2d3
emorrisonweeks <130721836+emorrisonweeks@users.noreply.github.com>
2024-01-17 14:39:11 -0600

    further tweaking

--- week1/exercises/ex2/startingp5.js
+++ week1/exercises/ex2/startingp5.js
@@ -2,9 +2,11 @@ function setup () {
     createCanvas (1000, 1000);
 }
 function draw () {
-    background (220);
+    background (	90.2, 90.2, 98);
     ellipse (50, 50, 80, 100);
 }
+window.prompt("ready to paint?") 
+
 function draw() {
     if (mouseIsPressed) {
       fill(127, 255, 212);
@@ -14,3 +16,6 @@ function draw() {
     }
     ellipse(mouseX, mouseY, 80, 80);
   }
+
+    
+  
