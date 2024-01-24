cd8dcf1cff47301ec3898d9ff74ca4371aff02f7
emorrisonweeks <130721836+emorrisonweeks@users.noreply.github.com>
2024-01-23 21:36:27 -0600

    Adding prompt and push

--- week2/exercises/ex1/moviearray.js
+++ week2/exercises/ex1/moviearray.js
@@ -1,2 +1,6 @@
 let myArr = ["the princess bride", "fantastic mister fox", "my neighbor totoro", "sabrina (1954)", "the nightmare before christmas"];
 console.log(myArr);
+let name = window.prompt("what is your favorite movie");
+alert ('Your response was: ' +name);
+myArr.push(prompt+name);
+console.log(myArr);
