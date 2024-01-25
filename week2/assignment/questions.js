cfec51dfd09e847aca6ab4afe0bc0f0c281f4417
emorrisonweeks <130721836+emorrisonweeks@users.noreply.github.com>
2024-01-24 21:14:29 -0600

    princess bride trivia setup

---
+++ week2/assignment/questions.js
@@ -0,0 +1,15 @@
+const questions = [
+    {question:"Fill in the blank to finish the quote. 'Hello. My name is Inigo Montoya. You killed my ____. Prepare to die." , answer: "father"},
+    {question:"Who wrote The Princess Bride?" , answer: "William Goldman"},
+    {question:"Which actor was almost cast in the role of Fezzik instead of Andre Roussimoff?" , answer: "Arnold Schwarzenegger"},
+    {question:"What type of injury did Mandy Patinkin sustain while attempting to hold back laughter during scenes with Billy Crystal?" , answer: "a bruised rib"},
+    {question:"What size is the rodent found in the fire swamp?" , answer: "unusual size"},
+    {question:"Fill in the blank to complete the quote. 'Stop rhyming, I mean it.' 'Anybody want a _____?'" , answer: "peanut"},
+    {question:"Which creatures in the film were not in the original novel?" , answer: "shrieking eels"},
+    {question:"Who were Miracle Max and his wife Valerie named after?" , answer: "William Goldman's parents"},
+    {question:"What are the names of the two rival kingdoms in the movie?" , answer: "Florin and Guilder"},
+    {question:"Which fictional vigilante inspired the costume for the Dread Pirate Roberts?" , answer: "Zorro"},
+];
+console.log(questions) 
+console.log(games) 
+const response = window.prompt("Can you answer the following Princess Bride trivia question?");
