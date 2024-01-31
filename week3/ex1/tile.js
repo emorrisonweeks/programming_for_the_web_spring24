e6e35419d389648fee42c09f0d934cb82fd92442
emorrisonweeks <130721836+emorrisonweeks@users.noreply.github.com>
2024-01-30 22:00:36 -0600

    tile

---
+++ week3/ex1/tile.js
@@ -0,0 +1,26 @@
+function setup() {
+    createCanvas(200, 200);
+}
+function createTile() {
+    translate(0, 0);
+    fill('purple');
+    rect(0, 0, 200, 200);
+    stroke("hotpink");
+    strokeWeight(5);
+    line(0, 0, 200, 200);
+    line(200, 0, 0, 200);
+    noStroke();
+    fill("lightblue");
+    circle(100, 50, 50);
+    circle(50, 100, 50);
+    circle(150, 100, 50);
+    circle(100, 150, 50);
+    noFill();
+    stroke("hotpink");
+    strokeWeight(5);
+    circle(100, 100, 100);
+}
+
+function draw() {
+    createTile();
+}
