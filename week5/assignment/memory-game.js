c37a8bc76ca31174c91d8f3ef25d006af9256ae5
emorrisonweeks <130721836+emorrisonweeks@users.noreply.github.com>
2024-02-13 21:56:15 -0600

    memory game

--- week5/assignment/memory-game.js
+++ week5/assignment/memory-game.js
@@ -1,9 +1,8 @@
-let myCard;
+let myCard; 
 function setup(){
     createCanvas(800, 600);
     background(0);
     let myCard = new Card();
-    myCard.show();
 }
 
 //to show whether card has been clicked
@@ -16,17 +15,21 @@ class Card {
         this.y = 100;
         this.width = 80;
         this.height = 100;
+        this.show();
     }
+
     show () {
         fill('rgb(57, 74, 9.9%, 9.9%');
         rect(this.x, this.y, this.width, this.height, 10);
     }
-}
 
-didHit(mouseX, mouseY){
-    if(mouseX >= this.x && mouseX <= this.x + this.width && mouseY >= this.y && mouseY <= this.y + this.height){
-        return true;
-    } else {
-        return false;
+    didHit (mouseX, mouseY) {
+        if (mouseX >= this.x && mouseX <= this.x + this.width && mouseY >= this.y && mouseY <= this.y + this.height) {
+            return true;
+        } else {
+            return false;
+        }
+            
     }
 }
+
